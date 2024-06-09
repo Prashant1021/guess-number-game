c6a49c7f8136562421976925d1c449a964f8b710
Prashant Shrestha <shresthaprashant703@gmail.com>
2024-06-09 14:15:43 +0545

    code for score commited

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 'use strict';
-let score=20
-let secretNumber=Math.trunc(Math.random()*20+1)
-document.querySelector('.number').textContent=secretNumber
+let score = 20;
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = secretNumber;
 
-document.querySelector('.check').addEventListener('click',()=>{
-    let guess=Number(document.querySelector('.guess').value)
-    if(!guess){
-        document.querySelector('.message').textContent="No Number"
-    }else if(guess===secretNumber){
-        document.querySelector('.message').textContent="🏆 correct Number"
-
-
-    } else if(guess>secretNumber){
-        document.querySelector('.message').textContent="📉 Too High"
-
-
-
-    }else if(guess<secretNumber){
-        document.querySelector('.message').textContent="📈 Too Low"
+document.querySelector('.check').addEventListener('click', () => {
+  let guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🏆 correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too High';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.score').textContent = 'you lost game';
     }
-
-})
-
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+        document.querySelector('.message').textContent = '📈 Too Low';
+        score = score - 1;
+        document.querySelector('.score').textContent = score;
+      } else {
+        document.querySelector('.score').textContent = 'you lost game';
+      }
+   
+  }
+});
